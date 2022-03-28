22c84760afcdffd623de2cc277cf8c8890f5831d
Manideep Alimi <manideep.a@comakeit.com>
2022-03-28 15:34:30 +0530

    Updated UI Changes

--- src/Components/Level2.js
+++ src/Components/Level2.js
@@ -4,69 +4,115 @@ import '../App.css';
 import axios from "axios";
 function Level2(props) {
     //console.log(props.dataToNext)
-    const [questions,setquestions]=useState([])
+    const [questions, setquestions] = useState([])
     const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [answers,setanswers]=useState(Array(10).fill(0));
     const [showScore, setShowScore] = useState(false);
     const [score, setScore] = useState(0);
-    const [option, setoption] = useState("");
-        const getquestions=()=>
-        {
-            useEffect(() => {
-                axios.get("http://localhost:5001/questions/"+props.dataToNext+"/2").then((res)=>
-                {   
-                   
-                    setquestions(res.data.questions);
-                }
-                )
-            }, [])
-           
-        }
+    const [level, setlevel] = useState(2);
+    const getquestions = () => {
+        useEffect(() => {
+            axios.get("http://localhost:5001/questions/" + props.dataToNext + "/" + level).then((res) => {
+                setquestions(...questions, res.data.questions);
+                // console.log(res.data.questions);
+            }
+            )
+        }, [])
+    }
     getquestions();
-    // console.log(questions);
-    const check = () => {
-        const answer = option
-        if (answer == questions[currentQuestion]["answers"][0]["text"]) {
-            setScore(score + 1)
-        }
-        // console.log(currentQuestion)
+    //console.log(questions);
+    const NextQuestion = () => {
         if (currentQuestion + 1 < questions.length)
             setCurrentQuestion(currentQuestion + 1);
-        else 
+        else {
             setShowScore(true);
+        }
+    }
+    const PrevQuestion = () => {
+            setCurrentQuestion(currentQuestion - 1);
 
     }
-    const setCurrentOption = (e) => {
-        setoption(e.target.value)
-        //console.log(e.target.value);
+    const setCurrentOption = index => e => {
+      let newarr=[...answers]
+      console.log(answers);
+      newarr[currentQuestion]=index
+      setanswers(newarr);
+    }
+    const validate=()=>
+    {    console.log(answers);
+        let cnt=0;
+        for(let i=0;i<answers.length;i++)
+        {   
+            //console.log(questions[i].answers[0]["text"],questions[i].options[answers[i]]["text"],answers[i])
+            if(questions[i].answers[0]["text"]===questions[i].options[answers[i]]["text"]){
+                cnt++;
+                // setScore(score+1);
+            }
+        }
+        setScore(cnt)
+        // console.log(cnt)
+        const percentage = (cnt / questions.length) * 100
+            console.log(cnt, percentage);
+            if (percentage >= 60) {
+                setlevel(2)   
+            }
+            setShowScore(true);
     }
     return (<>
-        <div>
+        <div className='container' >
             {showScore ? (<div className='score-section'>
                 You scored {score} out of {questions.length}
+                {level == 2 ?
+                    (<>
+                        <div className='mt-5 text-center'>Hurray ! You are Qualified for Level 2 </div>
+                        <Link to="/display/level2"><button>Level 2</button></Link>
+                    </>
+                    ) :
+                    (<>
+                        <div className='mt-2' style={{display:"block"}}>
+                            Sorry ! You are Not Qualified for Level 2 Please Try Again !!
+                        <Link to="/" ><button >Level-1</button></Link>
+                        </div>
+                    </>)}
             </div>) : (
-                <>
-                    <h2>Welcome to {props.dataToNext} quiz Level 2 </h2>
-                        {questions.map(function(d, idx){
-                            if(idx==currentQuestion) return (
-                        <>
-                            <div className='question-section'>
+                <>  <div style={{marginLeft:"30%",marginTop:"20%"}}>
+                    <h2 >Welcome to {props.dataToNext} quiz Level {level}</h2>
+                    {questions.map(function (d, idx) {
+                        if (idx == currentQuestion) return (
+                            <>
+                                <div className='question-section'>
                                     <div className='question-count'>
                                         <span>Question {currentQuestion + 1}</span>/{questions.length}
                                     </div>
-                            <div className='question-text'>{d.question}</div>
-                            </div>
-
-                            { d["options"].map((o, index) => (
-                                <> <div className="form-check">
-                                    <input className="form-check-input" onChange={setCurrentOption} type="radio" name="radio" value={o.text}></input>
-                                    <label className="form-check-label" for={index} >{o.text}</label>
+                                    <div className='question-text'><h4>{d.question}</h4></div>
                                 </div>
-                                </>
-                            ))}
-                        </>
+                                <div className='answer-section'>
+                                {d["options"].map((o, index) => (
+                                    <> <div className="form-check">
+                                        <input className="form-check-input" onChange={setCurrentOption(index)} type="radio" name="radio" value={o.text}  checked = {index==answers[currentQuestion]} ></input>
+                                        <label className="form-check-label" for={index} >{o.text}</label>
+                                    </div>
+                                    </>
+                                ))}
+                                </div>
+                                <div className='d-flex'>
+                                {
+                                    currentQuestion!=0 ?(
+                                    <button style={{marginLeft:"45%"}} type="button" className='btn btn-info mt-3' onClick={PrevQuestion}>Previous</button>):
+                                    (<div></div>)
+                                }
+                                {
+                                    currentQuestion!=(questions.length-1)?(
+                                    <button style={{marginLeft:"45%"}} type="button" className='btn btn-info mt-3' onClick={NextQuestion}>Next</button>):
+                                    (<button style={{marginLeft:"45%"}} type="button" className='btn btn-info mt-3' onClick={validate}>Submit</button>)
+                                }
+                                </div>
+                            </>
                         )
-                        })}
-                    <button ClassName="btn btn-primary" onClick={check}>Next</button>
+                    })
+                    }
+                    
+                </div>
                 </>
             )
             }
