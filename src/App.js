d3ea145bdb9a170fef754fbbb73019070fbd0a6c
LikhithaKoppula <likhitha.k@comakeit.com>
2022-04-11 10:18:38 +0530

    Updated code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Components/Home';
 import Level2 from './Components/Level2';
 import Next from './Components/Next';
 
+
 function App() {
   const [language, setlanguage] = useState("")
   /*const style=  {backgroundImage:"url(https://cdn.pixabay.com/photo/2018/02/27/15/40/background-3185765__340.jpg)",height:"100%",width:"100%","background-position": "center",
@@ -21,6 +22,7 @@ function App() {
         <Route exact path="/"  element={<Home dataToApp={dataToApp}/>}></Route>
         <Route exact path="/display/level1" element={<Next dataToNext={language}/>}></Route>
         <Route exact path="/display/level2" element={<Level2 dataToNext={language} level={2} />}></Route>
+        
         </Routes>
     </BrowserRouter>
     </div>
